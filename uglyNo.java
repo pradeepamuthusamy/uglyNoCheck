d4cb4a6362ec55925ed935ead8941c06c87f0356
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-04-09 18:40:08 +0530

    Add files via upload

---
+++ uglyNo.java
@@ -0,0 +1,25 @@
+//An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+
+//Given an integer n, return true if n is an ugly number.
+import java.util.Scanner;
+
+class uglyNo{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(callUgly(num));
+		
+		
+	}
+	public static boolean callUgly(int n){
+		if(n<=0) return false;
+		int [] arr = {2,3,5};
+		for(int factor:arr){
+			while(n % factor == 0){
+				n /= factor;
+			}
+		}
+		return n == 1;
+		
+	}
+}
